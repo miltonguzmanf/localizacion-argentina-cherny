d1239f662aff6df88e479e86d2a66a20564314fc
root <root@ip-172-31-35-43.us-west-2.compute.internal>
2021-07-21 20:57:48 +0000

    [FIX] Factura Electronica

--- l10n_ar_pos_electronic_invoiced/models/pos_order.py
+++ l10n_ar_pos_electronic_invoiced/models/pos_order.py
@@ -23,13 +23,13 @@ class PosOrder(models.Model):
                 raise UserError(_('Please provide a partner for the sale.'))
 
             move_vals = order._prepare_invoice_vals()
-            new_move = order._create_invoice(move_vals)
+            new_move = moves.sudo()\
+                            .with_context(default_type=move_vals['type'], force_company=order.company_id.id)\
+                            .create(move_vals)
+            message = _("This invoice has been created from the point of sale session: <a href=# data-oe-model=pos.order data-oe-id=%d>%s</a> ") % (order.id, order.name)
+            new_move.message_post(body=message)
             
-            #####################
-            ## Validacion AFIP ##
-            #####################
-            
-            order.action_post()
+            new_move.action_post()
 
             order.write({'account_move': new_move.id, 'state': 'invoiced'})
             new_move.sudo().with_context(force_company=order.company_id.id).post()
