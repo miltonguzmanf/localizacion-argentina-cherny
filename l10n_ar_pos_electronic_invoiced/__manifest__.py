22688815508071c304781f246ba270c1d3cf7fce
Milton Guzman <miltonguzmanf@gmail.com>
2021-07-21 17:28:25 -0300

    [ADD] Modulo Factura Electronica POS

---
+++ l10n_ar_pos_electronic_invoiced/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Factura Electronica desde POS',
+    'version': '13.0.1.0',
+    'category': 'Sales/Point Of Sale',
+    'sequence': 20,
+    'summary': 'Factura electronica desde POS',
+    'description': "",
+    'depends': ['point_of_sale','l10n_ar_afipws_fe'],
+    'installable': True,
+}
---
+++ l10n_ar_pos_electronic_invoiced/models/pos_order.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, tools, _
+from odoo.tools import float_is_zero
+from odoo.exceptions import UserError
+from odoo.http import request
+from odoo.osv.expression import AND
+import base64
+
+class PosOrder(models.Model):
+    _inherit = "pos.order"
+
+    def action_pos_order_invoice(self):
+        moves = self.env['account.move']
+
+        for order in self:
+            # Force company for all SUPERUSER_ID action
+            if order.account_move:
+                moves += order.account_move
+                continue
+
+            if not order.partner_id:
+                raise UserError(_('Please provide a partner for the sale.'))
+
+            move_vals = order._prepare_invoice_vals()
+            new_move = order._create_invoice(move_vals)
+            
+            #####################
+            ## Validacion AFIP ##
+            #####################
+            
+            order.action_post()
+
+            order.write({'account_move': new_move.id, 'state': 'invoiced'})
+            new_move.sudo().with_context(force_company=order.company_id.id).post()
+            moves += new_move
+
+        if not moves:
+            return {}
+
+        return {
+            'name': _('Customer Invoice'),
+            'view_mode': 'form',
+            'view_id': self.env.ref('account.view_move_form').id,
+            'res_model': 'account.move',
+            'context': "{'type':'out_invoice'}",
+            'type': 'ir.actions.act_window',
+            'nodestroy': True,
+            'target': 'current',
+            'res_id': moves and moves.ids[0] or False,
+        }
